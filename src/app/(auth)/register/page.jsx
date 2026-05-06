cdbe208286ecf36f7cb38d35ab5f2f1cf6e315dd
rsdrahi <rsdrahi13@gmail.com>
2026-05-06 15:48:20 +0600

    dragon project setup. daisyUI install. app routing(auth,main)folder created

---
+++ src/app/(auth)/register/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      This is Register Page
+    </div>
+  );
+};
+
+export default RegisterPage;
---
+++ src/app/(main)/news/[id]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewsDetailsPage = () => {
+  return (
+    <div>
+      This is News Details Page
+    </div>
+  );
+};
+
+export default NewsDetailsPage;
